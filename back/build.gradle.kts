101e6cfa07c6bd37df54397bc4fb2d78c3faba38
XyL1GaN4eG <trolloguy@mail.ru>
2025-01-06 01:14:02 +0300

    build: добавление зависимостей

--- back/build.gradle.kts
+++ back/build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     id("java")
+    id("io.freefair.lombok") version "8.10"
+    id("war")
 }
 
 group = "web.lab04"
@@ -10,10 +12,60 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("com.google.code.gson:gson:2.11.0")
+    // https://mvnrepository.com/artifact/jakarta.platform/jakarta.jakartaee-api
+    compileOnly("jakarta.platform:jakarta.jakartaee-api:11.0.0-M4")
+    // https://mvnrepository.com/artifact/org.postgresql/postgresql
+    implementation("org.postgresql:postgresql:42.7.4")
+    // https://mvnrepository.com/artifact/org.mongodb/mongodb-driver-sync
+    implementation("org.mongodb:mongodb-driver-sync:5.2.1")
+    // https://mvnrepository.com/artifact/org.slf4j/slf4j-api
+    implementation("org.slf4j:slf4j-api:2.0.16")
+    // https://mvnrepository.com/artifact/ch.qos.logback/logback-classic
+    testImplementation("ch.qos.logback:logback-classic:1.5.15")
+    // todo: возможно не будет нормально билдится или что то такое так что если вдруг проблемы
+    //  то разбираться надо сразу с ло(м)б(о)ком
+    // https://mvnrepository.com/artifact/org.projectlombok/lombok
+    compileOnly("org.projectlombok:lombok:1.18.36")
+
 }
 
-tasks.test {
-    useJUnitPlatform()
+tasks.withType<War> {
+    manifest {
+        attributes["Main-Class"] = "Main"
+    }
+
+    from("src/main/webapp") {
+        into("/")
+    }
+
+    val dependencies = configurations
+        .runtimeClasspath
+        .get()
+        .map(::zipTree)
+    from(dependencies)
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+}
+
+tasks.withType<JavaCompile> {
+    options.compilerArgs.addAll(arrayOf("--release", "17"))
+}
+
+tasks.compileJava {
+    options.encoding = "UTF-8"
+}
+
+tasks.javadoc {
+    options.encoding = "UTF-8"
+}
+
+tasks.create("deploy") {
+    dependsOn("war")
+
+    doLast {
+        exec {
+            workingDir(".")
+            commandLine("bash", "deploy.sh")
+        }
+    }
 }
